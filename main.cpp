536b8c238995ac59260474a8445bfccd9a563491
Michael <mbonilla5601@sdsu.edu>
2021-11-08 15:45:20 -0800

    All requirements complete. Ready for submission

--- main.cpp
+++ main.cpp
@@ -23,6 +23,9 @@ bool containsInvalidChar(std::string &token) {
         if (isdigit(c))
             return true;
     }
+    char endChar = token[token.size()-1];
+    if(ispunct(endChar))
+        return true;
 }
 
 void setCaps(std::string &token) {
@@ -33,52 +36,90 @@ void setCaps(std::string &token) {
         else
             token[i]= tolower(token[i], loc);
     }
-
-
 }
 
-int main() {
+void printPasswords(std::string *passwords){
+
+    for(int i = 0; i < 20; i++){
+        std::cout << passwords[i] << std::endl;
+    }
+}
+
+int getRandInt(int leftLim,int rightLim){
+    std::random_device seed;
+    std::mt19937 randomGenerator{seed()};
+    std::uniform_int_distribution<int> distribution{leftLim,static_cast<int>(rightLim)};
+    return distribution(randomGenerator);
+}
+
+template <std::size_t N>
+bool arrayContains(int const (&array)[N], int lookingFor){
+
+    for (auto& x : array){
+        if(x == lookingFor)
+            return true;
+    }
+    return false;
+}
+
+int main(int argc, char* argv[]) {
+    //setting up variables for use
     std::string line;
-    std::string delimter = " ";
-    std::string const delimiters{" "};
     std::string potentialToken;
-    std::string goodToken;
-    std::ifstream myfile("./input.txt");
     std::set<std::string> passwordTokens;
     std::string passwordString;
+    int usedIterations[5];
+    std::string allPasswords[20];
 
+    //will read input.txt, unless command line arg is provided.
+    std::ifstream myfile("./input.txt");
+    if(argc > 1)
+        std::ifstream myfile(argv[1]);
+
+    //text files have gibberish at the beginning, this skips it.
     SkipBOM(myfile);
+
+    //reads the file and adds token to a set.
     if (myfile.is_open()) {
         while (std::getline(myfile, line)) {
             std::istringstream iss(line);
             do {
                 iss >> potentialToken;
                 unsigned int tokenSize = potentialToken.size();
+                //check to make sure token meets criteria
                 if (tokenSize <= 6 && tokenSize >= 3 && !containsInvalidChar(potentialToken)) {
                     setCaps(potentialToken);
                     passwordTokens.insert(potentialToken);
-                    if(passwordTokens.size() == 207)
-                        int i = 10;
                 }
             } while (iss);
         }
         myfile.close();
     }
 
-    std::random_device seed;
-    std::mt19937 randomGenerator{seed()};
-    std::uniform_int_distribution<int> distribution{1,static_cast<int>(passwordTokens.size())};
-    int numOfIterations = distribution(randomGenerator);
+    //get random num to iterate through set, this will be a password token
+    int numOfIterations = getRandInt(1, static_cast<int>( passwordTokens.size()));
+    usedIterations[0] = numOfIterations;
 
     auto iterator = passwordTokens.begin();
-    for(int i = 0; i < 4; i++) {
-        for (int i = 0; i < numOfIterations; i++) {
-            iterator++;
+    for(int i = 0; i < 20; i++) {
+        for (int j = 0; j < 4; j++) {
+            for (int k = 0; k < numOfIterations; k++) {
+                iterator++;
+            }
+            passwordString.append(" " + *iterator);
+            iterator = passwordTokens.begin();
+
+            //checks that the same token isnt used twice in the same password
+            do {
+                numOfIterations = getRandInt(1, static_cast<int>( passwordTokens.size()));
+            } while (arrayContains(usedIterations, numOfIterations));
+            usedIterations[j+1] = numOfIterations;
         }
-        passwordString.append(" " + *iterator);
-        iterator = passwordTokens.begin();
-        numOfIterations = distribution(randomGenerator);
+        passwordString.append("1");
+        passwordString.erase(0, 1);
+        allPasswords[i] = passwordString;
+        passwordString.clear();
     }
-    std::cout << passwordString;
+    printPasswords(allPasswords);
     return 0;
 }
